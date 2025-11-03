1380acfe0b32e4de25604e27afdbed2e43f336b5
Sepvaldi <sepvalditunisia@gmail.com>
2025-11-03 20:04:36 +0700

    Add files via upload

---
+++ LatihanUKL4.java
@@ -0,0 +1,24 @@
+//Buatlah program menghitung volume tabung dengan menggunakan konsep fungsi. Sertakan parameter dalam fungsi tersebut 
+
+import java.util.Scanner;
+
+public class LatihanUKL4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double jariJari = 0;
+        double tinggi = 0;
+        double phi = 3.14;
+
+        //Inputan
+        System.out.println("=====Menghitung Volume Tabung=====");
+        System.out.println();
+        System.out.println("Masukkan jari-jari");
+        jariJari = input.nextDouble();
+        System.out.println("Masukkan tinggi");
+        tinggi = input.nextDouble();
+
+        //Output
+        System.out.println("Volume tabung adalah: " + jariJari* jariJari * tinggi * phi);
+        
+    }
+}
